a9a1dbcd60f365234944367abf136d3928eedfdb
Lucas Nascimento <lluckymou@gmail.com>
2021-07-05 13:42:27 -0300

    Slingshot changes and a few fixes

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    [SerializeField]
+    Material LitMaterial;
+
+    Material original;
+
+    void Awake() => original = GetComponent<MeshRenderer>().material;
+
+    void OnCollisionEnter(Collision c) =>
+        GetComponent<MeshRenderer>().material = LitMaterial;
+
+    void OnCollisionExit(Collision c) =>
+        GetComponent<MeshRenderer>().material = original;
+}
